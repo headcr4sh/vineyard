f6b8654c042bd2dc6662b49fd407141c9cf0bb34
Benjamin P. Jung <headcr4sh@googlemail.com>
2012-02-25 17:08:28 +0100

    Prepare window listener that will restore window location/size

--- vineyard/src/main/java/com/googlecode/vineyard/ui/MainFrame.java
+++ vineyard/src/main/java/com/googlecode/vineyard/ui/MainFrame.java
@@ -1,12 +1,18 @@
 package com.googlecode.vineyard.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Rectangle;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.prefs.Preferences;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JToolBar;
+
+import com.googlecode.vineyard.VineyardConfiguration;
 
 /**
  * 
@@ -17,6 +23,8 @@ public class MainFrame extends JFrame {
 
 	/** @see java.io.Serializable */
 	private static final long serialVersionUID = 7365980744906239688L;
+
+	private final Preferences prefs = Preferences.userNodeForPackage(getClass());
 
 	public MainFrame() {
 		super();
@@ -29,6 +37,16 @@ public class MainFrame extends JFrame {
 
 		// TODO Add WindowListener and handle exit event correctly
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				final Rectangle frameBounds = MainFrame.this.getBounds();
+				prefs.putInt("bounds.x", frameBounds.x);
+				prefs.putInt("bounds.y", frameBounds.y);
+				prefs.putInt("bounds.width", frameBounds.width);
+				prefs.putInt("bounds.height", frameBounds.height);
+			}
+		});
 
 		setTitle("Vineyard");
 		setName("VineyardMainFrame");
